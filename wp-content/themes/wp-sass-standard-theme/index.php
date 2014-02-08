c19a542925784af5f77ec66968a19f4e12b06724
Jeremy Pry <jeremy.pry@gmail.com>
2014-02-08 15:35:36 -0500

    Adjust spacing for readability

--- wp-content/themes/wp-sass-standard-theme/index.php
+++ wp-content/themes/wp-sass-standard-theme/index.php
@@ -13,12 +13,12 @@
 get_header(); ?>
 <div class="inner contentpage">
 	<div class="colonewide">
-		<?php if ( function_exists('yoast_breadcrumb') ) {
-			yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+		<?php if ( function_exists( 'yoast_breadcrumb' ) ) {
+			yoast_breadcrumb( '<p id="breadcrumbs">', '</p>' );
 		} ?>
 	</div><!-- / .colonewide -->
 	<div class="coltwothirds leftcol">
-		<h1><?php esc_attr(the_title()); ?></h1>
+		<h1><?php esc_attr( the_title() ); ?></h1>
 		<?php 
 		// the query
 		$args = array ( 'post_type' => 'post', 'orderby' => 'date', 'order' => 'DSC', 'paged' => $paged , 'posts_per_page' => '10' );
@@ -29,13 +29,13 @@ get_header(); ?>
 			  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 			  	<li>
 			  		<a href="<?php the_permalink() ?>" class="bloglink">
-					<?php if(has_post_thumbnail()) {
+					<?php if ( has_post_thumbnail() ) {
 						echo '<span class="blogimg">';
 						the_post_thumbnail();
 						echo '</span>';
 					} ?>
 				    <h2><?php the_title(); ?></h2></a>
-				    <p class="date">POSTED: <?php echo get_the_date('j-M-Y'); ?></p>
+				    <p class="date">POSTED: <?php echo get_the_date( 'j-M-Y' ); ?></p>
 					<?php the_excerpt(); ?>
 				</li>
 			  <?php endwhile; ?>
