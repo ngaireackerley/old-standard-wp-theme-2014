3b2a626e697a8c0b3325dbd7d8c87f6d89fba45b
Jeremy Pry <jeremy.pry@gmail.com>
2014-02-08 15:11:41 -0500

    Adjust code spacing

--- wp-content/themes/wp-sass-standard-theme/footer.php
+++ wp-content/themes/wp-sass-standard-theme/footer.php
@@ -20,8 +20,8 @@
 			<div class="threecols leftcol">
 				<p>&copy; Client
 				<?php
-					$this_year = date('Y');
-					if ($this_year != 2013) {
+					$this_year = date( 'Y' );
+					if ( $this_year != 2013 ) {
 						echo '2013 - ' . $this_year;
 					} else {
 						echo $this_year . ', all rights reserved.';
@@ -30,31 +30,27 @@
 			<div class="coltwothirds rightcol">
 				<!-- social media -->
 				<div class="socialmedia">
-					<?php $homepage = 4;
-					if(get_field('youtube_link', $homepage))
-					{
-					    echo '<a href="' . esc_url(get_field('youtube_link', $homepage)) . '"><img src="' .get_template_directory_uri() . '/images/youtube-20px.png" alt="Twitter" /></a>';
-					} 
-					if(get_field('linkedin_page_link', $homepage))
-					{
-					    echo '<a href="' . esc_url(get_field('linkedin_page_link', $homepage)) . '"><img src="' .get_template_directory_uri() . '/images/linkedin-20px.png" alt="Twitter" /></a>';
+					<?php
+					$homepage = 4;
+					if ( get_field( 'youtube_link', $homepage ) ) {
+						echo '<a href="' . esc_url( get_field( 'youtube_link', $homepage ) ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/youtube-20px.png" alt="Twitter" /></a>';
 					}
-					if(get_field('google_plus_link', $homepage))
-					{
-					    echo '<a href="' . esc_url(get_field('google_plus_link', $homepage)) . '"><img src="' .get_template_directory_uri() . '/images/google-plus-20px.png" alt="Twitter" /></a>';
-					} 
-					if(get_field('facebook_grouppage_link', $homepage))
-					{
-					    echo '<a href="' . esc_url(get_field('facebook_grouppage_link', $homepage)) . '"><img src="' .get_template_directory_uri() . '/images/facebook-20px.png" alt="Twitter" /></a>';
-					} 
-					if(get_field('twitter_user_page_link', $homepage))
-					{
-					    echo '<a href="' . get_field('twitter_user_page_link', $homepage) . '"><img src="' .get_template_directory_uri() . '/images/twitter-t-20px.png" alt="Twitter" /></a>';
-					} 
-					if(get_field('rss_feed_link', $homepage))
-					{
-					    echo '<a href="' . esc_url(get_field('rss_feed_link', $homepage)) . '"><img src="' .get_template_directory_uri() . '/images/rss-20px.png" alt="Twitter" /></a>';
-					} ?>
+					if ( get_field( 'linkedin_page_link', $homepage ) ) {
+						echo '<a href="' . esc_url( get_field( 'linkedin_page_link', $homepage ) ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/linkedin-20px.png" alt="Twitter" /></a>';
+					}
+					if ( get_field( 'google_plus_link', $homepage ) ) {
+						echo '<a href="' . esc_url( get_field( 'google_plus_link', $homepage ) ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/google-plus-20px.png" alt="Twitter" /></a>';
+					}
+					if ( get_field( 'facebook_grouppage_link', $homepage ) ) {
+						echo '<a href="' . esc_url( get_field( 'facebook_grouppage_link', $homepage ) ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/facebook-20px.png" alt="Twitter" /></a>';
+					}
+					if ( get_field( 'twitter_user_page_link', $homepage ) ) {
+						echo '<a href="' . get_field( 'twitter_user_page_link', $homepage ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/twitter-t-20px.png" alt="Twitter" /></a>';
+					}
+					if ( get_field( 'rss_feed_link', $homepage ) ) {
+						echo '<a href="' . esc_url( get_field( 'rss_feed_link', $homepage ) ) . '"><img src="' . get_stylesheet_directory_uri() . '/images/rss-20px.png" alt="Twitter" /></a>';
+					}
+					?>
 				</div><!-- / .socialmedia -->
 				<!-- menu -->
 				<?php wp_nav_menu( array( 'theme_location' => 'footer', 'before' => '<span class="divider">&nbsp;|&nbsp;</span>' ) ); ?>
