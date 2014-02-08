73de56fb0170bc83fa439b0e9afa2506ac43c59b
Jeremy Pry <jeremy.pry@gmail.com>
2014-02-08 12:59:11 -0500

    Remove unused function twentyten_admin_header_style()

--- wp-content/themes/wp-sass-standard-theme/functions.php
+++ wp-content/themes/wp-sass-standard-theme/functions.php
@@ -80,31 +80,6 @@ function twentyten_setup() {
 }
 endif;
 
-if ( ! function_exists( 'twentyten_admin_header_style' ) ) :
-/**
- * Styles the header image displayed on the Appearance > Header admin panel.
- *
- * Referenced via add_custom_image_header() in twentyten_setup().
- *
- * @since Twenty Ten 1.0
- */
-function twentyten_admin_header_style() {
-?>
-<style type="text/css" id="twentyten-admin-header-css">
-/* Shows the same border as on front end */
-#headimg {
-	border-bottom: 1px solid #000;
-	border-top: 4px solid #000;
-}
-/* If header-text was supported, you would style the text with these selectors:
-	#headimg #name { }
-	#headimg #desc { }
-*/
-</style>
-<?php
-}
-endif;
-
 /**
  * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
  *
