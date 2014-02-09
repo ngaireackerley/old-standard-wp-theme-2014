1e2edb53f4de8d8dad3dd47e29e6be793ead5a46
Jeremy Pry <jeremy.pry@gmail.com>
2014-02-08 20:39:04 -0500

    Fix formatting

--- wp-content/themes/wp-sass-standard-theme/page-blog-twocols-2-1.php
+++ wp-content/themes/wp-sass-standard-theme/page-blog-twocols-2-1.php
@@ -8,13 +8,14 @@ get_header(); ?>
 		} ?>
 	</div><!-- / .colonewide -->
 	<div class="coltwothirds leftcol">
-		<h1><?php esc_attr(the_title()); ?></h1>
+		<h1><?php esc_attr( the_title() ); ?></h1>
 		<?php if(has_post_thumbnail()) {
 			the_post_thumbnail();
 		} ?>
 		<?php 
 		// the query
-		$args = array ( 'post_type' => 'post', 'orderby' => 'date', 'order' => 'DSC', 'paged' => $paged , 'posts_per_page' => '10' );
+		$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+		$args = array ( 'post_type' => 'post', 'orderby' => 'date', 'order' => 'DSC', 'paged' => $paged, 'posts_per_page' => 10 );
 		$the_query = new WP_Query( $args );
 		if ( $the_query->have_posts() ) : ?>
 			<ul class="bloglist">
@@ -22,13 +23,13 @@ get_header(); ?>
 			  <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
 			  	<li>
 			  		<a href="<?php the_permalink() ?>" class="bloglink">
-					<?php if(has_post_thumbnail()) {
+					<?php if ( has_post_thumbnail() ) {
 						echo '<span class="blogimg">';
 						the_post_thumbnail();
 						echo '</span>';
 					} ?>
 				    <h2><?php the_title(); ?></h2></a>
-				    <p class="date">POSTED: <?php echo get_the_date('j-M-Y'); ?></p>
+				    <p class="date">POSTED: <?php echo get_the_date( 'j-M-Y' ); ?></p>
 					<?php the_excerpt(); ?>
 				</li>
 			  <?php endwhile; ?>
